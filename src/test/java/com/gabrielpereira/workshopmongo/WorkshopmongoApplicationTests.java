5b533fb0630a4b0e50032846dd8adb6a13bf92d6
Gabriel Pereira de Sousa <gabrielpereira.gn@gmail.com>
2024-12-05 09:02:50 -0300

    Projeto criado

---
+++ src/test/java/com/gabrielpereira/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielpereira.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
